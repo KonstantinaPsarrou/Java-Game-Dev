bd313a87ae6dfa78be2eb99f344f9ff545de3977
KonstantinaPsarrou <81778154+KonstantinaPsarrou@users.noreply.github.com>
2023-03-02 21:01:32 +0200

    Add files via upload

---
+++ src/inputt.java
@@ -0,0 +1,32 @@
+import java.io.Reader;
+import java.io.FileReader;
+
+public class inputt {
+    public static void main(String[] args) {
+
+        // Creates an array of character
+        char[] array = new char[100];
+
+        try {
+            // Creates a reader using the FileReader
+            Reader input = new FileReader("SCENARIO-1.txt");
+  
+
+            
+            // Checks if reader is ready 
+            System.out.println("Is there data in the stream?  " + input.ready());
+
+            // Reads characters
+            input.read(array);
+            System.out.println("Data in the stream:");
+            System.out.println(array);
+
+            // Closes the reader
+            input.close();
+        }
+
+        catch(Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
